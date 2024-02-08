74034dca316bf928ec73d5f9d6f8376d783e66a6
ozzzih <dhwlgus3031@gmail.com>
2024-02-08 18:05:01 +0900

    2024/02/08

---
+++ ch05/Test02_test.java
@@ -0,0 +1,54 @@
+package com.ruby.java.ch05;
+
+import java.util.Scanner;
+
+public class Test02_test {
+	static String sumCase(int n) {
+		
+		if(n==10) {
+			System.out.println("(1,1,1)");
+		}
+		else {
+			for(int i = 1 ; i<(n-10); i++) {
+				int a = n-10-(2*i);
+				if((a==0) && (i<11)) {
+					System.out.println("("+(i+1)+", 1, 1)");
+				}
+				else {
+					for(int j=1; j<a; j++) {
+						int b=a-(3*j);
+						if((b==0) && (j<11)) {
+							System.out.println("("+(i+1)+", "+(j+1)+", 1)");
+						}
+						for(int k=1; k<b; k++) {
+							int c=b-(5*k);
+							if((c==0) && (k<11)) {
+								System.out.println("("+(i+1)+", "+(j+1)+", "+(k+1)+")");
+								break;
+							}
+						}
+					}
+				}
+				
+			}
+		}
+		
+		return "끝";
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("10~100사이의 임의의 값을 입력하세요: ");
+		int num = sc.nextInt();
+		if(num<10 ||num>100) {
+			System.out.println("!입력 값이 틀렸습니다!");
+			System.out.println("10~100사이의 임의의 값을 입력하세요: ");
+			num = sc.nextInt();
+		}
+		System.out.println(sumCase(num));
+		
+		
+
+	}
+
+}
