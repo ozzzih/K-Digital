257545516dddaabf439a1aad4633496a0f7850de
오지콩 <90295119+ozzzih@users.noreply.github.com>
2024-02-14 00:19:56 +0900

    2024/02/13_수정

--- ch06/Matrix2.java
+++ ch06/Matrix2.java
@@ -1,112 +1,99 @@
 package com.ruby.java.ch06;
 
-public class Matrix2 {
+import java.util.Random;
+import java.util.Arrays;
+public class Matrix2
+{
+    private int s; 
+    private int r; 
+    private int c;
+    private int x[][];
+    private int[] array;
+    
+    public Matrix2(int[] array, int s, int r, int c){
+        this.s=s;
+        this.r=r;
+        this.c=c;
+        this.x = new int[r][c];
+        this.array = array;
+        
+        //행렬에 대입
+        for(int i=0; i<r; i++) { //3
+			for(int j=0; j<c; j++) { //4
+			    int idx = i*c+j;
+		        x[i][j]=array[s+idx];
+			}
+		}
+    }
+    public void print(){
+		for(int i=0; i<r; i++) {
+			for(int j=0; j<c; j++) {
+				if(x[i][j]<10)
+					System.out.print("0");
+				System.out.print(x[i][j]+"   ");
+				if((j+1)%c==0)
+					System.out.println();
+			}
+		}
+    }
+    public static Matrix2 multiple(Matrix2 a, Matrix2 b){
+        int array[]=a.array;
+        Matrix2 x = new Matrix2(array, 0, a.r, b.c);
+        for(int i=0; i<a.r; i++) {
+			for(int j=0; j<b.c; j++) {
+				for(int k=0; k<a.c; k++){
+				    x.x[i][j]+=a.x[i][k]*b.x[k][j];
+				}
+					
+			}
+		}
+		return x;
+    }
+    
+    public static Matrix2 sum(Matrix2 a, Matrix2 b){
+        int array[]=a.array;
+        Matrix2 x = new Matrix2(array, 0, a.r, a.c);
+        for(int i=0; i<a.r; i++) {
+			for(int j=0; j<a.c; j++) {
 
+				x.x[i][j]=a.x[i][j]+b.x[i][j];
+			}
+		}
+		return x;
+    }
+    
 	public static void main(String[] args) {
-		int A1[][] = new int[3][4];//난수 입력
-		int B1[][] = new int[3][4];//난수 입력
-		int A2[][] = new int[3][4];
-		int B2[][] = new int[4][3];//난수 입력
-		int A3[][] = new int[4][3];
-		int B3[][] = new int[3][3];
-		//C=A*B
-		//E=A+B
-		//F=A의 전치 행렬
-		//난수 생성하기
+	    int[] array = new int[24];
+	    //난수 생성하기
 		Random rnd = new Random();
+		for(int i=0; i<24; i++){
+		    array[i] = rnd.nextInt(50)+1; //0발생 제외
+		}
+		System.out.println("배열 생성: "+Arrays.toString(array));
+		Arrays.sort(array);
+		System.out.println("오름차순 정렬: "+Arrays.toString(array));
 		
-		// 행렬 만들기
-		// 3*4행렬 만들기
-		for(int i=0; i<A.length; i++) { //3
-			for(int j=0; j<A[i].length; j++) { //4
-				A[i][j] = rnd.nextInt(10)+1;
-				D[i][j] = rnd.nextInt(10)+1;
-			}
-		}
-		// 4*5행렬 만들기
-		for(int i=0; i<B.length; i++) { //4
-			for(int j=0; j<B[i].length; j++) { //5
-				B[i][j] = rnd.nextInt(10)+1;
-			}
-		}
-		//끝
-		
-		
-		
-		System.out.println("*****[A]****");
-		for(int i=0; i<A.length; i++) {
-			for(int j=0; j<A[i].length; j++) {
-				if(A[i][j]<10)
-					System.out.print("0");
-				System.out.print(A[i][j]+"   ");
-				if((j+1)%A[i].length==0)
-					System.out.println();
-				
-			}
-		}
-		System.out.println("*****[B]*****");
-		for(int i=0; i<B.length; i++) {
-			for(int j=0; j<B[i].length; j++) {
-				if(B[i][j]<10)
-					System.out.print("0");
-				System.out.print(B[i][j]+"   ");
-				if((j+1)%B[i].length==0)
-					System.out.println();
-				
-			}
-		}
-		System.out.println("#######[문제 1]########");
-		//문제: c=a*b
-		for(int i=0; i<A.length; i++) {
-			for(int j=0; j<B[0].length; j++) {
-				for(int k=0; k<A[0].length; k++)
-					C[i][j]+=A[i][k]*B[k][j];
-			}
-		}
-		System.out.println("****[곱셈의 결과: C]****");
-		for(int i=0; i<C.length; i++) {
-			for(int j=0; j<C[i].length; j++) {
-				if(C[i][j]<100)
-					System.out.print("0");
-				System.out.print(C[i][j]+"   ");
-				if((j+1)%C[i].length==0)
-					System.out.println();
-				
-			}
-		}
-		System.out.println("#######[문제 2]########");
-		for(int i=0; i<A.length; i++) {
-			for(int j=0; j<A[0].length; j++) {
+		Matrix2 mA1 = new Matrix2(array, 0, 3, 4);
+        Matrix2 mB1 = new Matrix2(array, 12, 3, 4);
+        System.out.println("A1행렬");
+        mA1.print();
+        System.out.println("B1행렬");
+        mB1.print();
+        Matrix2 mC1 = Matrix2.sum(mA1, mB1);
+        System.out.println("A1+B1 결과");
+        mC1.print();
+        
+        // 행렬곱 s, r, c
+        Matrix2 mA2 = new Matrix2(array, 0, 3, 4);
+        Matrix2 mB2 = new Matrix2(array, 12, 4, 3);
+        System.out.println("A2행렬");
+        mA1.print();
+        System.out.println("B2행렬");
+        mB1.print();
+        System.out.println("A2*B2 결과");
+        Matrix2 mC2 = Matrix2.multiple(mA2, mB2);
+        mC2.print();
 
-				E[i][j]+=A[i][j]+B[i][j];
-			}
-		}
-		System.out.println("****[덧셈의 결과: E]****");
-		for(int i=0; i<E.length; i++) {
-			for(int j=0; j<E[i].length; j++) {
-				if(E[i][j]<10)
-					System.out.print("0");
-				System.out.print(E[i][j]+"   ");
-				if((j+1)%E[i].length==0)
-					System.out.println();
-			}
-		}
-		System.out.println("#######[문제 3]########");
-		System.out.println("****[전치행렬 결과: F]****");
-		for(int i=0; i<A.length; i++) {
-			for(int j=0; j<A[0].length; j++) {
-				F[j][i]=A[i][j];
-			}
-		}
-		for(int i=0; i<F.length; i++) {
-			for(int j=0; j<F[i].length; j++) {
-				if(F[i][j]<10)
-					System.out.print("0");
-				System.out.print(F[i][j]+"   ");
-				if((j+1)%F[i].length==0)
-					System.out.println();
-			}
-		}
-		
+
 	}
 }
