28e79ab03a0dfed7590b245b514c06c56c4f54a6
ozzzih <dhwlgus3031@gmail.com>
2024-02-26 18:02:14 +0900

    2024/02/26

--- class2/06/06.js
+++ class2/06/06.js
@@ -1,14 +1,27 @@
-const handleClick = ()=>{
+const handleClick = (i)=>{
     const bts = document.querySelectorAll("section img");
-    const btsNum = [1,1,1];
+    
+    
+    const n = Math.floor(Math.random() * 6)+1;
+    bts[0].setAttribute("src", `./img/${n}.png`);
+    bts[1].setAttribute("src", `./img/${i}.png`);
+    if(n==i){
+        document.querySelector("#result").innerHTML=`<h1>⭕맞음</h1>`;
+    }else{
+        document.querySelector("#result").innerHTML=`<h1>❌틀림</h1>`;
+    }
+
+    
+
+    //const btsNum = [1,1,1];
 
     //for of
-    for(let [idx, bt] of bts.entries()){
-        const n = Math.floor(Math.random() * 6)+1;
-        bt.setAttribute("src", `./img/${n}.png`);
-        btsNum[idx] = n;
-        console.log(btsNum);
-    }    
+    // for(let [idx, bt] of bts.entries()){
+    //     const n = Math.floor(Math.random() * 6)+1;
+    //     bt.setAttribute("src", `./img/${n}.png`);
+    //     btsNum[idx] = n;
+    //     console.log(btsNum);
+    // }    
 
     //for of=>파이썬의 for in
     // for(let bt of bts){
